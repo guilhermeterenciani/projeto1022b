d19e6588bbe99af4629c193cd68984bb612d6e11
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-08-24 10:56:08 -0400

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,4 @@
 import mysql from 'mysql2/promise';
-
-console.log("TERE LINDO");
 // Create the connection to database
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -8,7 +6,20 @@ const connection = mysql.createConnection({
   database: 'banco1022b',
 });
 connection
-.then() // Se deu certo
+.then((conn)=>{ // Se deu certo
+    // Preparar uma query para execução.
+    const queryPreparada = conn.prepare("SELECT * FROM produtos");
+    queryPreparada
+    .then()
+    .catch((err)=>{
+        if(err.code==='ER_NO_SUCH_TABLE'){
+            console.log("ERRO: VOCÊ DEVE CRIAR A TABELA PRODUTOS NO WORKBENCH")
+        }
+        else{
+            console.log("Erro não tratado:",err);
+        }
+    })
+}) 
 .catch((erro)=>{ // Se deu Errado
     if(erro.code==='ECONNREFUSED'){
         console.log("ERRO: FAVOR LIGA O LARAGON!")
