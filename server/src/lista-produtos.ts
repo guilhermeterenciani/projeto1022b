44be5d1e8ce1c5f16eed92927cea7d198de061da
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-08-26 11:07:58 -0400

    Listagem de dados no banco de dados com uma classe.

---
+++ server/src/lista-produtos.ts
@@ -0,0 +1,65 @@
+import mysql, { RowDataPacket } from 'mysql2/promise';
+type Output = {
+    id:number,
+    nome:string,
+    descricao:string,
+    preco: number,
+    imagem:string
+}
+class ListaProdutos{
+    execute(){
+        const connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            database: 'banco1022b',
+          });
+          return connection
+          .then((conn)=>{ // Se deu certo
+              // Preparar uma query para execução.
+              const queryPreparada = conn.prepare("SELECT * FROM produtos");
+              return queryPreparada
+              .then((query)=>{
+                  const queryExecutada = query.execute([])
+                  return queryExecutada
+                  .then((result)=>{
+                      const [linhas ,campus] = result
+                      const dados = linhas as RowDataPacket[]
+                      const listaProdutos:Output[] = []
+                      for(let i=0;i<dados.length;i++){
+                          console.log(dados[i].id,dados[i].nome)
+                          const produto ={
+                            id:dados[i].id,
+                            nome: dados[i].nome,
+                            descricao: dados[i].descricao,
+                            preco: dados[i].preco,
+                            imagem: dados[i].imagem,
+                          }
+                          return listaProdutos.push(produto)
+                      }
+                  })
+                  .catch((err)=>console.log("Erro:",err))
+              })
+              .catch((err)=>{
+                  if(err.code==='ER_NO_SUCH_TABLE'){
+                      console.log("ERRO: VOCÊ DEVE CRIAR A TABELA PRODUTOS NO WORKBENCH")
+                  }else if(err.code==='ER_PARSE_ERROR'){
+                      console.log("ERRO: VOCÊ DIGITOU ALGO ERRADO NA QUERY, CONFIRA A ESCRITA, VIRGULAS,NOME DAS COLUNAS E POSIÇÃO DAS PALAVRAS CHAVES.")
+                  }
+                  else{
+                      console.log("Erro não tratado:",err);
+                  }
+              })
+          }) 
+          .catch((erro)=>{ // Se deu Errado
+              if(erro.code==='ECONNREFUSED'){
+                  console.log("ERRO: FAVOR LIGA O LARAGON!")
+              }else if(erro.code==='ER_BAD_DB_ERROR'){
+                  console.log("ERRO: você não criou o banco de dados 'banco1022B' no workbench!")
+              }else{
+                  console.log(erro)
+              }
+          })
+    }
+}
+
+export default ListaProdutos;
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,44 +1,5 @@
-import mysql, { RowDataPacket } from 'mysql2/promise';
-// Create the connection to database
+import ListaProdutos from "./lista-produtos";
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'banco1022b',
-});
-connection
-.then((conn)=>{ // Se deu certo
-    // Preparar uma query para execução.
-    const queryPreparada = conn.prepare("SELECT * FROM produtos");
-    queryPreparada
-    .then((query)=>{
-        const queryExecutada = query.execute([])
-        .then((result)=>{
-            const [linhas ,campus] = result
-            const dados = linhas as RowDataPacket[]
-            for(let i=0;i<dados.length;i++){
-                console.log(dados[i].id,dados[i].nome)
-            }
-        })
-        .catch((err)=>console.log("Erro:",err))
-    })
-    .catch((err)=>{
-        if(err.code==='ER_NO_SUCH_TABLE'){
-            console.log("ERRO: VOCÊ DEVE CRIAR A TABELA PRODUTOS NO WORKBENCH")
-        }else if(err.code==='ER_PARSE_ERROR'){
-            console.log("ERRO: VOCÊ DIGITOU ALGO ERRADO NA QUERY, CONFIRA A ESCRITA, VIRGULAS,NOME DAS COLUNAS E POSIÇÃO DAS PALAVRAS CHAVES.")
-        }
-        else{
-            console.log("Erro não tratado:",err);
-        }
-    })
-}) 
-.catch((erro)=>{ // Se deu Errado
-    if(erro.code==='ECONNREFUSED'){
-        console.log("ERRO: FAVOR LIGA O LARAGON!")
-    }else if(erro.code==='ER_BAD_DB_ERROR'){
-        console.log("ERRO: você não criou o banco de dados 'banco1022B' no workbench!")
-    }else{
-        console.log(erro)
-    }
-})
+const objListaProdutos = new ListaProdutos();
+const produtos = objListaProdutos.execute();
+console.log("Produtos:",produtos)
