9db78a9976e2329f28138c4a54730a830dc211f0
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-08-27 11:18:29 -0400

    Update lista-produtos.ts

--- server/src/lista-produtos.ts
+++ server/src/lista-produtos.ts
@@ -8,57 +8,57 @@ type Output = {
 }
 class ListaProdutos{
     async execute(){
-        const connection = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            database: 'banco1022b',
-          });
-         
-           const queryPreparada = await connection.prepare("SELECT * FROM produtos");
-              return queryPreparada
-              .then((query)=>{
-                  const queryExecutada = query.execute([])
-                  return queryExecutada
-                  .then((result)=>{
-                      const [linhas ,campus] = result
-                      const dados = linhas as RowDataPacket[]
-                      const listaProdutos:Output[] = []
-                      for(let i=0;i<dados.length;i++){
-                          console.log(dados[i].id,dados[i].nome)
-                          const produto ={
-                            id:dados[i].id,
-                            nome: dados[i].nome,
-                            descricao: dados[i].descricao,
-                            preco: parseFloat(dados[i].preco),
-                            imagem: dados[i].imagem,
-                          }
-                          listaProdutos.push(produto)
-                      }
-                      return listaProdutos
+        try{
+            const connection = await mysql.createConnection({
+                host: 'localhost',
+                user: 'root',
+                database: 'banco1022b',
+            });
+            const queryPreparada = connection.prepare("SELECT * FROM produtos");
+                return queryPreparada
+                .then((query)=>{
+                    const queryExecutada = query.execute([])
+                    return queryExecutada
+                    .then((result)=>{
+                        const [linhas ,campus] = result
+                        const dados = linhas as RowDataPacket[]
+                        const listaProdutos:Output[] = []
+                        for(let i=0;i<dados.length;i++){
+                            console.log(dados[i].id,dados[i].nome)
+                            const produto ={
+                                id:dados[i].id,
+                                nome: dados[i].nome,
+                                descricao: dados[i].descricao,
+                                preco: parseFloat(dados[i].preco),
+                                imagem: dados[i].imagem,
+                            }
+                            listaProdutos.push(produto)
+                        }
+                        return listaProdutos
 
-                  })
-                  .catch((err)=>console.log("Erro:",err))
-              })
-              .catch((err)=>{
-                  if(err.code==='ER_NO_SUCH_TABLE'){
-                      console.log("ERRO: VOCÊ DEVE CRIAR A TABELA PRODUTOS NO WORKBENCH")
-                  }else if(err.code==='ER_PARSE_ERROR'){
-                      console.log("ERRO: VOCÊ DIGITOU ALGO ERRADO NA QUERY, CONFIRA A ESCRITA, VIRGULAS,NOME DAS COLUNAS E POSIÇÃO DAS PALAVRAS CHAVES.")
-                  }
-                  else{
-                      console.log("Erro não tratado:",err);
-                  }
-              })
-          }) 
-          .catch((erro)=>{ // Se deu Errado
-              if(erro.code==='ECONNREFUSED'){
-                  console.log("ERRO: FAVOR LIGA O LARAGON!")
-              }else if(erro.code==='ER_BAD_DB_ERROR'){
-                  console.log("ERRO: você não criou o banco de dados 'banco1022B' no workbench!")
-              }else{
-                  console.log(erro)
-              }
-          })
+                    })
+                    .catch((err)=>console.log("Erro:",err))
+                })
+                .catch((err)=>{
+                    if(err.code==='ER_NO_SUCH_TABLE'){
+                        console.log("ERRO: VOCÊ DEVE CRIAR A TABELA PRODUTOS NO WORKBENCH")
+                    }else if(err.code==='ER_PARSE_ERROR'){
+                        console.log("ERRO: VOCÊ DIGITOU ALGO ERRADO NA QUERY, CONFIRA A ESCRITA, VIRGULAS,NOME DAS COLUNAS E POSIÇÃO DAS PALAVRAS CHAVES.")
+                    }
+                    else{
+                        console.log("Erro não tratado:",err);
+                    }
+                })
+        }
+        catch(erro){ // Se deu Errado
+            if(erro.code==='ECONNREFUSED'){
+                console.log("ERRO: FAVOR LIGA O LARAGON!")
+            }else if(erro.code==='ER_BAD_DB_ERROR'){
+                console.log("ERRO: você não criou o banco de dados 'banco1022B' no workbench!")
+            }else{
+                console.log(erro)
+            }
+        }
     }
 }
 
