b00216c5c473a08f4b880209830ebded7892b429
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-08-28 10:10:36 -0400

    Tarefa: Criar o código de listagem de usuários com testes.

--- server/banco.sql
+++ server/banco.sql
@@ -8,6 +8,16 @@ CREATE TABLE IF NOT EXISTS produtos(
     preco DECIMAL(10,2),
     imagem VARCHAR(300)
 );
+CREATE TABLE IF NOT EXISTS usuarios(
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    idade INT,
+    cpf VARCHAR(50),
+    rg VARCHAR(50),
+    endereco VARCHAR(100)
+    estado_civil ENUM("solteiro,casado")
+);
 INSERT INTO produtos VALUES
 (1,"Iphone","Celular RUIM",5000.50,"SEM IMAGEM");
-  
+
+INSERT INTO usuarios VALUES (1,"João",18,"036.547.382-10","002.874.325","Rua das flores, Bairro dos Planetas, Número 10, Naviraí - MS","casado");
---
+++ server/src/lista-usuario.ts
@@ -0,0 +1,6 @@
+import mysql, { RowDataPacket } from 'mysql2/promise';
+import 'dotenv/config'
+class ListaUsuario{
+    
+}
+export default ListaUsuario;
