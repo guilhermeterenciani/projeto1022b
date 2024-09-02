518ff7fda1e0fcbcb721b30f0fb9b8a0da1549a3
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-09-02 11:44:10 -0400

    Update lista-produtos.test.ts

--- server/src/lista-produtos.test.ts
+++ server/src/lista-produtos.test.ts
@@ -8,23 +8,24 @@ beforeEach(async()=>{
         user: process.env.DB_USER,
         database: process.env.DB_BANCO,
     });
-    await connection.query("DELETE FROM produtos WHERE id=?",[1])
+    await connection.query("DELETE FROM produtos")
     await connection.query("INSERT INTO produtos VALUES (?,?,?,?,?)",
         ["1","Iphone","Celular RUIM","5000.50","SEM IMAGEM"])
 })
 test("Deve listar produtos do banco de dados",async()=>{
     //GIVEN   -> DADO ALGO INICIAL
     const listaProdutos = new ListaProdutos()
-    const listaPreCadastrada =
-    {
-        id: 1,
-        nome: 'Iphone',
-        descricao: 'Celular RUIM',
-        preco: 5000.50,
-        imagem: 'SEM IMAGEM'
-    }
+    const listaPreCadastrada =[
+        {
+            id: 1,
+            nome: 'Iphone',
+            descricao: 'Celular RUIM',
+            preco: 5000.50,
+            imagem: 'SEM IMAGEM'
+        }
+    ]
     //WHEN    -> QUANDO EU EXECUTAR ALGO
     const produtosDoBanco = await listaProdutos.execute()
     //THEN    -> EU ESPERO QUE ISSO ACONTEÇA
-    expect(produtosDoBanco).toContainEqual(listaPreCadastrada)
+    expect(produtosDoBanco).toEqual(listaPreCadastrada)
 })
