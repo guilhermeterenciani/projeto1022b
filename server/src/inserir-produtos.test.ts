965d1fe8b83e69e75b10f2602e415fbc424a5ee1
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-09-02 10:42:20 -0400

    Create inserir-produtos.test.ts

---
+++ server/src/inserir-produtos.test.ts
@@ -0,0 +1,30 @@
+import {test,expect, afterEach} from 'vitest'
+import ListaProdutos from './lista-produtos'
+import mysql from 'mysql2/promise';
+import 'dotenv/config'
+afterEach(async()=>{
+    const connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        database: process.env.DB_BANCO,
+    });
+    await connection.query("DELETE FROM produtos WHERE id=?",[2])
+})
+test("Deve inserir um produto no banco de dados",async()=>{
+    //GIVEN
+    const produto = {
+        id:2,
+        nome: "Samsung",
+        descricao:"Celular melhor que Iphone",
+        preco:1500.50,
+        imagem:"SEM IMAGEM"
+    }
+    //WHEN  => Quando eu fizer algo.
+    const inserirProdutos = new InserirProdutos()
+    const produtoDoBanco = await inserirProdutos.executar(produto)
+    //THEN
+    const listaProdutos = new ListaProdutos()
+    const produtosBanco = listaProdutos.execute()
+    expect(produtoDoBanco).toEqual(produto)
+    expect(produtoDoBanco).toContainEqual(produto)
+})
