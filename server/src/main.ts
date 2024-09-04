02fddce3013d82edc7eeed3e21bc45c54a50f7d9
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-09-04 08:55:17 -0400

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@
 import Express , {Request,Response} from 'express';
 
 import ListaProdutos from "./lista-produtos";
+import InserirProdutos from './inserir-produtos';
 const app = Express();
 
 app.get("/",(req:Request,res:Response)=>{
@@ -11,6 +12,17 @@ app.get("/produtos", async (req:Request,res:Response)=>{
     const objListaProdutos = new ListaProdutos();
     const produtos = await objListaProdutos.execute();
     res.send(produtos).status(200);
+})
+app.post("/produtos", async (req:Request,res:Response)=>{
+    const {id,nome,descricao,preco,imagem} = req.body
+    const produto = {id,nome,descricao,preco,imagem}
+    const objInserirProdutos = new InserirProdutos()
+    try{
+        const produtoInserido =await objInserirProdutos.execute(produto)
+        res.status(201).send(produtoInserido);
+    }catch(e){
+        res.status(400).send({mensagem:"Não foi possível cadastrar"});
+    }
 })
 
 app.listen(8000,()=>{
