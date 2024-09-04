3bac19da44a5622e96e8b5f0eb8387947166f173
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-09-04 10:14:10 -0400

    Código que lista os dados do banco na aplicação web

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,20 +1,25 @@
 
 import Express , {Request,Response} from 'express';
-
+import Cors from 'cors'
 import ListaProdutos from "./lista-produtos";
 import InserirProdutos from './inserir-produtos';
 const app = Express();
+
+app.use(Cors())
+app.use(Express.json())
 
 app.get("/",(req:Request,res:Response)=>{
     res.send("Respondido.").status(200);
 })
 app.get("/produtos", async (req:Request,res:Response)=>{
     const objListaProdutos = new ListaProdutos();
+    //O tipo do produto tem que ser o mesmo do fetch
     const produtos = await objListaProdutos.execute();
     res.send(produtos).status(200);
 })
 app.post("/produtos", async (req:Request,res:Response)=>{
     const {id,nome,descricao,preco,imagem} = req.body
+    //Esse produto tem que ser igual ao do fetch
     const produto = {id,nome,descricao,preco,imagem}
     const objInserirProdutos = new InserirProdutos()
     try{
